18d13efd5aeeb7664c09144fa2117a679b35d994
sourabh-2012 <sourabhsuttarwar123@gmail.com>
2026-01-27 18:33:48 +0530

    Fib number

---
+++ LC/Practice/FibonacciNumber.java
@@ -0,0 +1,24 @@
+package LC.Practice;
+
+public class FibonacciNumber {
+
+    static void Fibonacci(INT N)
+    {
+        int n1=0 , n2=1;
+
+        for(int i =0 ; i < N; i++){
+            System.out.print(n1 + " ");
+
+            //swap
+            int n3 = n2 + n1;
+            n1 = n2 ;
+            n2 = n3 ;
+        }
+    }
+    
+    public static void main(String args[])
+    {
+        int N = 10;
+        Fibonacci(N);
+    }
+}
