aa86775e25c1c5f66261777a91560ff0804607c1
sourabh-2012 <sourabhofficialemail123@gmail.com>
2026-02-01 20:51:53 +0530

    Optimized approach added to the class

--- LC/Arrays/ThreeSum.java
+++ LC/Arrays/ThreeSum.java
@@ -4,6 +4,7 @@ package LC.Arrays;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
 
 public class ThreeSum {
@@ -38,4 +39,50 @@ public class ThreeSum {
         }
         return res;        
     }
+
+/*  ---------------------------   */
+    /*Optimized */
+
+    class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>>lst=new ArrayList<>();
+        HashSet<List<Integer>>set=new HashSet<>();
+    
+        
+        Arrays.sort(nums);
+
+        for(int i=0;i<nums.length-2;i++){
+            
+            
+            int left=i+1,right=nums.length-1;
+            while(left<right){
+                List<Integer>subset=new ArrayList<>();
+                if(nums[i]+nums[left]+nums[right]==0){
+                    subset.add(nums[i]);
+                    subset.add(nums[left]);
+                    subset.add(nums[right]);
+                    set.add(subset);
+                    
+                    left++;
+                    right--;
+
+                }
+                if(nums[i]+nums[left]+nums[right]>0){
+                    right--;
+                }
+                if(nums[i]+nums[left]+nums[right]<0){
+                    left++;
+                }
+
+
+            }
+              
+        }
+        for(List<Integer> values:set){
+            lst.add(values);
+        }
+        return lst;
+    }
+}
+
 }
