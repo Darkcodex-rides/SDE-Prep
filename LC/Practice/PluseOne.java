2eee1ea48c5891a6f26449e954c570fcb4a0ad11
sourabh-2012 <sourabhofficialemail123@gmail.com>
2026-02-03 23:36:30 +0530

    Run out of energy - Pluse One

---
+++ LC/Practice/PluseOne.java
@@ -0,0 +1,21 @@
+package LC.Practice;
+
+
+public class PluseOne {
+
+    public int[] plusOne(int[] digits) {
+        for (int i = digits.length - 1; i >= 0; i--) {
+            if (digits[i] + 1 != 10) {
+                digits[i] += 1;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+        
+        int[] newDigits = new int[digits.length + 1];
+        newDigits[0] = 1;
+        return newDigits;        
+    }
+
+    
+}
